3293712081a69b00e9d3d194ebecb300a9cec1da
ilzfor <66512876+ilzfor@users.noreply.github.com>
2020-07-11 21:04:47 +0800

    跑步 滑行向量

---
+++ New Unity Project run game 2D/Assets/script/Learnif.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+
+public class Learnif : MonoBehaviour
+{
+    public bool test;
+    private void Start()
+    {
+        //如果(布林值){程式內容}
+        //作用:當布林值為true才會執行{}程式內容
+        if (test)
+        {
+            print("我是判斷式");
+        }
+	}
+    public bool podo;
+    private void Update()
+    {
+        //當布林值為true會執行if(){}程式內容
+        //當布林值為false會執行else{}程式內容
+        if (podo)
+        {
+            print("開門");
+        }
+        else
+        {
+            print("關門");
+        }
+    }
+}
--- New Unity Project run game 2D/Assets/script/player.cs
+++ New Unity Project run game 2D/Assets/script/player.cs
@@ -8,7 +8,7 @@ public class player : MonoBehaviour
      *多行註解
      *
      */
-#region 欄位區域
+    #region 欄位區域
     //命名規則
     //1.有意義的名稱
     //2.不要用數字開頭
@@ -30,13 +30,13 @@ public class player : MonoBehaviour
     //提示 Tooltip
     //範圍 Range
 
-    [Header("速度"), Tooltip("腳色移動速度"), Range(10, 1500)]
+    [Header("速度"), Tooltip("腳色移動速度"), Range(1, 1500)]
     public int speed = 50;
     [Tooltip("腳色血量")]
     public float HP = 999.9f;
     [Tooltip("金幣")]
     public int coin;
-    [Tooltip("跳躍高度"),Range(100, 200)]
+    [Tooltip("跳躍高度"), Range(10, 1000)]
     public int heignt = 500;
     [Tooltip("跳躍音效")]
     public AudioClip sounjump;
@@ -51,19 +51,47 @@ public class player : MonoBehaviour
 
     [Header("動畫控制器")]
     public Animator ani;
+    [Header("膠囊碰撞器")]
+    public CapsuleCollider2D cc2d;
+    [Header("剛體")]
+    public Rigidbody2D rig;
+    public bool isGround;
     #endregion
-# region 方法區域
+    #region 方法區域
+    /// <summary>
+    /// 跑步
+    /// </summary>
+    private void Move()
+    {
+        if (rig.velocity.magnitude < 5)
+        {
+            //剛體,添加推力(二維向量)
+            rig.AddForce(new Vector2(speed, 0));
+        }
+
+    }
     /// <summary>
     /// 腳色跳躍的高度速度
     /// </summary>
-    private void jump()
+    private void Jump()
     {
         //布林值=輸入,取得按鍵(按鍵代碼列舉,左邊ctrl)
 
         bool key2 = Input.GetKey(KeyCode.Space);
+        // 顛倒運算子!
+        //作用:將布林值變成相反
+        //!true---false
+        ani.SetBool("跳躍", !isGround);
 
-        ani.SetBool("跳躍",key2);
-
+        //如果在地板上
+        if (isGround)
+        {
+            if (key2)
+            {
+                isGround = false;//不在地板上
+                rig.AddForce(new Vector2(0, heignt));//剛體,添加推力(二維向量) 
+            }
+        }
     }
     /// <summary>
     /// 腳色滑行時的速度和體積變化
@@ -71,9 +99,22 @@ public class player : MonoBehaviour
     private void slide()
     {
 
-       bool key =Input.GetKey(KeyCode.LeftControl);
+        bool key = Input.GetKey(KeyCode.LeftControl);
 
-        ani.SetBool("滑行",key);
+        ani.SetBool("滑行", key);
+        if (key)
+        {
+            cc2d.offset = new Vector2(-0.328f, -1.44f);//位移
+
+            cc2d.size = new Vector2(1.925f, 2.018f);//尺寸
+        }
+        else
+        {
+            cc2d.offset = new Vector2(-0.328f, -0.4425f);//位移
+
+            cc2d.size = new Vector2(1.925f, 4.013f);//尺寸
+        }
+
     }
     /// <summary>
     /// 腳色攻擊的素質
@@ -101,7 +142,7 @@ public class player : MonoBehaviour
     /// </summary>
     private void hti()
     {
-        
+
     }
     #endregion
     #region 事件區域
@@ -110,16 +151,39 @@ public class player : MonoBehaviour
     //初始化:
     private void Start()
     {
-      
-       
+
+
     }
     //更新 Update
     //播放遊戲後一秒直行約60次-60FPS
     //移動、監聽玩家鍵盤、滑鼠與觸控
     private void Update()
     {
-        jump();
         slide();
     }
+    /// <summary>
+    /// 一秒固定執行50次 有剛體就用這個執行
+    /// </summary>
+    private void FixedUpdate()
+    {
+
+        Jump();
+        Move();
+    }
+    /// <summary>
+    /// 碰撞事件:碰到物件開始執行一次
+    /// </summary>
+    /// <param name="collision">碰到物件的碰撞資料</param>
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //如果碰到物件的名稱等於"地板"
+        if (collision.gameObject.name == "地板")
+
+        {
+            //是否在地板上=是
+            isGround = true;
+        }
+    }
+
     #endregion
 }
